87ab75d693318c9b82471c142678824c629e2759
Erick Gonzalez <ericksinbox@gmail.com>
2021-02-26 11:51:39 -0500

    created login and register endpoints

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,30 @@
+const Users = require('../users/users-model.js');
+
 const router = require('express').Router();
+// const Users = require('../')
+
+
+router.get('/users', (req, res) => {
+  Users.getAll()
+  .then(users => {
+    res.status(200).json(users)
+  })
+});
 
 router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+  // res.end('implement register, please!');
+  const { username, password } = req.body;
+  const user = req.body;
+  console.log(user);
+
+  Users.add(user)
+
+  .then(res => {
+    res.status(200).json("Welcome to the Dad Joke's API!")
+  })
+  .catch(err => {
+    res.status(401).json(err.message)
+  })
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -29,30 +52,56 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+  const { username, password } = req.body;
+  const user = req.body
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
+  Users.findByUsername(user.username)
+  .then(u => {
+    res.status(200).json(u)
+  })
+  .catch(err => {
+    res.status(401).json(err.message)
+  })
 
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
 
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+})
 
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
-});
+// router.post('/login', (req, res) => {
+//   const { username, password } = req.body;
+//   const user = req.body;
+//   console.log(user.username);
+
+//   Users.findByUsername(user.username)
+
+//   .then(user => {
+//     res.status(200).json(user)
+//   })
+//   .catch(err => {
+//     res.status(401).json(err.message)
+
+//   /*
+//     IMPLEMENT
+//     You are welcome to build additional middlewares to help with the endpoint's functionality.
+
+//     1- In order to log into an existing account the client must provide `username` and `password`:
+//       {
+//         "username": "Captain Marvel",
+//         "password": "foobar"
+//       }
+
+//     2- On SUCCESSFUL login,
+//       the response body should have `message` and `token`:
+//       {
+//         "message": "welcome, Captain Marvel",
+//         "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+//       }
+
+//     3- On FAILED login due to `username` or `password` missing from the request body,
+//       the response body should include a string exactly as follows: "username and password required".
+
+//     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+//       the response body should include a string exactly as follows: "invalid credentials".
+//   */
+// });
 
 module.exports = router;
---
+++ api/users/users-model.js
@@ -0,0 +1,32 @@
+const db = require('../../data/dbConfig.js');
+
+module.exports = {
+    add,
+    // findById,
+    findByUsername,
+    getAll
+}
+
+
+function getAll() {
+    return db('users')
+}
+
+// function findById(id) {
+//     return db('users')
+//     .select("id", "username", "password")
+//     .where({ id })
+//     .first()
+// }
+
+function findByUsername(username) {
+    console.log(`username received ${username}`)
+    return db('users')
+    .select("id", "username", "password")
+    .where({ username });
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user);
+    return db('users').where({ id }).first();
+}
